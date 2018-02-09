6ad5e0143dc552fe2fca387cef3f910fc30d7ecd
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-02-09 16:56:06 -0300

    validate form

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {NgForm} from "@angular/forms";
 
 /**
  * Generated class for the ShoppingListPage page.
@@ -15,11 +16,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ShoppingListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShoppingListPage');
+  onAddItem(form: NgForm) {
+    console.log(form);
   }
 
 }
