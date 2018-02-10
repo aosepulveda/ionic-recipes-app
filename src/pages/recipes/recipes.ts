69785bfcb114879d47b941dd210cbcf226b8da64
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-02-10 09:02:46 -0300

    first version list recipes

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {EditRecipePage} from "../edit-recipe/edit-recipe";
+import {RecipesService} from "../../services/recipes";
+import {Recipe} from "../../models/recipe";
 
 @IonicPage()
 @Component({
@@ -8,14 +10,25 @@ import {EditRecipePage} from "../edit-recipe/edit-recipe";
   templateUrl: 'recipes.html',
 })
 export class RecipesPage {
+  recipes: Recipe[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private recipesService: RecipesService) {
+  }
+
+  ionViewWillEnter() {
+    this.loadRecipes();
   }
 
   onNewRecipe() {
     this.navCtrl.push(EditRecipePage, {
       mode: 'New'
     });
+  }
+
+  loadRecipes() {
+    this.recipes = this.recipesService.getRecipes();
   }
 
 }
