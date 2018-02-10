b810020e7d3304c9f933820f1b21ec7594548249
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-02-10 09:28:49 -0300

    added recipe edit page

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {EditRecipePage} from "../edit-recipe/edit-recipe";
 import {RecipesService} from "../../services/recipes";
 import {Recipe} from "../../models/recipe";
+import {RecipePage} from "../recipe/recipe";
 
 @IonicPage()
 @Component({
@@ -27,8 +28,11 @@ export class RecipesPage {
     });
   }
 
-  onLoadRecipe() {
-
+  onLoadRecipe(index: number, recipe: Recipe) {
+    this.navCtrl.push(RecipePage, {
+      index: index,
+      recipe: recipe
+    });
   }
 
   loadRecipes() {
